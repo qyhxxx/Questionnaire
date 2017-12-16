6d3046af7e4f2c2a8ec64bb971a917ed022116c1
qyhxxx <512202508@qq.com>
2017-12-16 14:09:55 +0800

    登录应该可以了

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,7 +12,7 @@ class LoginController extends Controller {
     public static function construct() {
         $app_key = "xVNxrf0yMuy7Cgs3pqdB";
         $app_id = "8";
-        $sso = new sso($app_id, $app_key, false);
+        $sso = new Api($app_id, $app_key);
         return $sso;
     }
 
@@ -25,7 +25,7 @@ class LoginController extends Controller {
 
     public static function storage($token) {
         $sso = self::construct();
-        $userinfo = $sso->getUserInfo($token);
+        $userinfo = $sso->fetchUserInfo($token);
         if ($userinfo->status) {
             $result = $userinfo->result;
             $data['user_number'] = $result->user_number;
