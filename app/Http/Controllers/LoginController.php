c48db37f964eb07404982d1e390341a66ce75e4e
qyhxxx <512202508@qq.com>
2017-12-16 19:03:49 +0800

    h

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -26,7 +26,9 @@ class LoginController extends Controller {
     public static function storage($token) {
         $sso = self::construct();
         $userinfo = $sso->fetchUserInfo($token);
-        dd($userinfo);
+        dd($userinfo->status);
+        dd($userinfo->result->user_number);
+        dd($userinfo->result->twt_name);
         if ($userinfo->status) {
             $result = $userinfo->result;
             $data['user_number'] = $result->user_number;
