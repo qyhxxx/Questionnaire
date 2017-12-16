8909dd3b91f1f99dca1bea74df03dd61c5ed247e
qyhxxx <512202508@qq.com>
2017-12-16 19:13:46 +0800

    g

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -31,12 +31,13 @@ class LoginController extends Controller {
             echo $result->user_number . "1 <br>";
             $data['user_number'] = $result->user_number;
             $data['twt_name'] = $result->twt_name;
+            echo $result->user_number . "2 <br>";
             Usr::add($data);
             $data['type'] = 0;
             $data['token'] = $token;
-            echo $result->user_number . "2 <br>";
-            session(['data' => $data]);
             echo $result->user_number . "3 <br>";
+            session(['data' => $data]);
+            echo $result->user_number . "4 <br>";
             exit;
             return 1;
         }
