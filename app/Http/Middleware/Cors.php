faee4b3ae17b4e8e5df552663279adc6888e406d
qyhxxx <512202508@qq.com>
2017-12-20 02:24:19 +0800

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,8 +16,8 @@ class Cors
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', 'https://survey.twtstudio.com/');
-//        $response->header('Access-Control-Allow-Origin', 'http://localhost:8080');
+        //$response->header('Access-Control-Allow-Origin', 'https://survey.twtstudio.com/');
+        $response->header('Access-Control-Allow-Origin', 'http://localhost:8080');
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, multipart/form-data, application/json');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
         $response->header('Access-Control-Allow-Credentials', 'true');
