3f10a421a1abf0371f0501e4379b4ae409b7ca89
qyhxxx <512202508@qq.com>
2018-01-01 22:53:53 +0800

    update getDataMiddleWare

--- app/Http/Middleware/GetDataMiddleware.php
+++ app/Http/Middleware/GetDataMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use App\Editor;
 use App\Http\Controllers\LoginController;
+use App\Question;
 use App\Questionnaire;
 use Closure;
 
@@ -18,29 +19,38 @@ class GetDataMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $qnid = $request->route('qnid');
-        $questionnaire = Questionnaire::getQuestionnaire($qnid);
-        if ($questionnaire->recovery_at != null && time() > $questionnaire->recovery_at) {
-            Questionnaire::updateByQnid($qnid, ['status' => 2]);
+        if ($request->route('qnid') != null) {
+            $qnid = $request->route('qnid');
+        } else if ($request->route('qid') != null) {
+            $qid = $request->route('qid');
+            $qnid = Question::getQuestionByQid($qid)->qnid;
+        } else {
+            $qnid = null;
         }
-        if ($questionnaire->status != 1) {
-            if ($request->session()->has('data')) {
-                $twt_name = $request->session()->get('data')['twt_name'];
-                $hasPower = Editor::hasPower($qnid, $twt_name);
-                if (!$hasPower) {
+        if ($qnid != null) {
+            $questionnaire = Questionnaire::getQuestionnaire($qnid);
+            if ($questionnaire->recovery_at != null && time() > $questionnaire->recovery_at) {
+                Questionnaire::updateByQnid($qnid, ['status' => 2]);
+            }
+            if ($questionnaire->status != 1) {
+                if ($request->session()->has('data')) {
+                    $twt_name = $request->session()->get('data')['twt_name'];
+                    $hasPower = Editor::hasPower($qnid, $twt_name);
+                    if (!$hasPower) {
+                        return response()->json([
+                            'info' => 0
+                        ]);
+                    }
+
+                } else {
                     return response()->json([
-                        'status' => 0
+                        'info' => 0
                     ]);
                 }
-
-            } else {
-                return response()->json([
-                    'status' => 0
-                ]);
-            }
-        } else if ($questionnaire->ischecked || $questionnaire->verifiedphone) {
-            if (!$request->session()->has('data')) {
-                LoginController::login();
+            } else if ($questionnaire->ischecked || $questionnaire->verifiedphone) {
+                if (!$request->session()->has('data')) {
+                    LoginController::login();
+                }
             }
         }
         return $next($request);
