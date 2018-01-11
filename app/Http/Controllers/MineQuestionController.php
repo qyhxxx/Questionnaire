2073d11cf61c572058392e5361389f398536f297
CacacaLalala <1055400347@qq.com>
2018-01-11 19:06:24 +0800

    .

--- app/Http/Controllers/MineQuestionController.php
+++ app/Http/Controllers/MineQuestionController.php
@@ -112,6 +112,7 @@ class MineQuestionController extends Controller
     //问卷展开[概述、设置]
     public function overview($qnid){
         $questionnaire_data = Questionnaire::getdata($qnid);
+        dd($questionnaire_data);
 //        if ($questionnaire_data['recovery_at'] != null) {
 //            $today_at = Carbon::now();
 //            if ($questionnaire_data['recovery_at'] <= $today_at) {
@@ -175,7 +176,6 @@ class MineQuestionController extends Controller
                 }
             }
         }
-        dd($answer_ques);
 //        $answer_sub = array_values($answer);
 //        foreach ($answer_sub as $key => $val){
 //            $answer_ques[$key] = array_values($answer_sub[$key]);
