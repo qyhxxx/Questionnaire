1a35950c2b8442148a3738a04e4d66a4707ee3a6
unknown <you@example.com>
2018-05-18 19:16:59 +0800

    test

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -159,6 +159,6 @@ class ManagerController extends Controller{
 
     public function deleteQuestionnaire($qnid){
         $delete_questionnaire = Questionnaire::deleteQuestionnaire($qnid);
-        return
+        dd($delete_questionnaire);
     }
 }
