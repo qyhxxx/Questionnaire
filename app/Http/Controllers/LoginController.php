00b7e272dabb6efcfffe3091081824e076a6fa3e
unknown <you@example.com>
2018-06-10 23:26:30 +0800

    update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -55,7 +55,7 @@ class LoginController extends Controller {
         }
         else {
             return response()->json([
-                'status' => 1
+                'status' => 0
             ]);
         }
     }
